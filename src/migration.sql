9cc0bf8668c94e41de689f815239e1c19aaf7e21
Raaffael <roblesjonathan96@gmail.com>
2022-12-08 13:43:05 -0700

    added seed and migration files

---
+++ src/migration.sql
@@ -0,0 +1,4 @@
+INSERT INTO university (name,category,applicationSubmitted) VALUES ('Western Governors University','target',false);
+INSERT INTO university (name,category,applicationSubmitted) VALUES ('a','target',false);
+INSERT INTO university (name,category,applicationSubmitted) VALUES ('b','target',false);
+INSERT INTO university (name,category,applicationSubmitted) VALUES ('c','target',false);
---
+++ src/seed.sql
@@ -0,0 +1,6 @@
+CREATE TABLE university (
+    school_id SERIAL PRIMARY KEY,
+    name TEXT,
+    category VARCHAR(30),
+    applicationSubmitted BOOLEAN
+);
